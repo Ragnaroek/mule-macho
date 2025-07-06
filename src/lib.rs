a737183c04412fba76f494dde9cf1e8dee92df10
Michael Bohn <spaceodyssey@gmx.de>
2025-07-06 17:59:50 +0200

    complete header parsing

--- src/lib.rs
+++ src/lib.rs
@@ -5,50 +5,98 @@ pub struct Macho {
     header: Header,
 }
 
-/*
-struct mach_header_64 {
-------    uint32_t	magic;		/* mach magic number identifier */
-    int32_t		cputype;	/* cpu specifier */
-    int32_t		cpusubtype;	/* machine specifier */
-    uint32_t	filetype;	/* type of file */
-    uint32_t	ncmds;		/* number of load commands */
-    uint32_t	sizeofcmds;	/* the size of all the load commands */
-    uint32_t	flags;		/* flags */
-    uint32_t	reserved;	/* reserved */
-};
-*/
 pub const MAGIC_HEADER: u32 = 0xfeedfacf;
 
 #[derive(Serialize)]
 pub struct Header {
     cpu_type: CPUType,
+    cpu_sub_type: CPUSubType,
+    file_type: FileType,
+    no_cmds: usize,
+    size_of_cmds: usize,
+    flags: Vec<HeaderFlag>,
 }
-
-/*
-#define CPU_TYPE_MC680x0        ((cpu_type_t) 6)
-#define CPU_TYPE_X86            ((cpu_type_t) 7)
-#define CPU_TYPE_I386           CPU_TYPE_X86            /* compatibility */
-#define CPU_TYPE_X86_64         (CPU_TYPE_X86 | CPU_ARCH_ABI64)
-
-/* skip CPU_TYPE_MIPS		((cpu_type_t) 8)	*/
-/* skip                         ((cpu_type_t) 9)	*/
-#define CPU_TYPE_MC98000        ((cpu_type_t) 10)
-#define CPU_TYPE_HPPA           ((cpu_type_t) 11)
-#define CPU_TYPE_ARM            ((cpu_type_t) 12)
-#define CPU_TYPE_ARM64          (CPU_TYPE_ARM | CPU_ARCH_ABI64)
-#define CPU_TYPE_ARM64_32       (CPU_TYPE_ARM | CPU_ARCH_ABI64_32)
-#define CPU_TYPE_MC88000        ((cpu_type_t) 13)
-#define CPU_TYPE_SPARC          ((cpu_type_t) 14)
-#define CPU_TYPE_I860           ((cpu_type_t) 15)
- */
 
 const CPU_ARCH_ABI64: i32 = 0x01000000;
 
 #[repr(i32)]
-#[derive(Serialize)]
+#[derive(Serialize, Copy, Clone, PartialEq)]
 pub enum CPUType {
     X86_64 = 7 | CPU_ARCH_ABI64,
     ARM64 = 12 | CPU_ARCH_ABI64,
+}
+
+#[repr(i32)]
+#[derive(Serialize)]
+pub enum CPUSubType {
+    ARM_ALL = 0,
+}
+
+#[repr(u32)]
+#[derive(Serialize)]
+pub enum FileType {
+    MH_OBJECT = 0x1,   /* relocatable object file */
+    MH_EXECUTED = 0x2, /* demand paged executable file */
+}
+
+#[repr(u32)]
+#[derive(Serialize, Copy, Clone)]
+pub enum HeaderFlag {
+    MH_NOUNDEFS = 0x01, /* the object file has no undefined references */
+    MH_INCRLINK = 0x02, /* the object file is the output of an incremental link against a base file
+                        and can't be link edited again */
+    MH_DYLDLINK = 0x4, /* the object file is input for the dynamic linker and can't be staticly
+                       link edited again */
+    MH_BINDATLOAD = 0x8, /* the object file's undefined references are bound by the dynamic
+                         linker when loaded. */
+    MH_PREBOUND = 0x10, /* the file has its dynamic undefined references prebound. */
+    MH_SPLIT_SEGS = 0x20, /* the file has its read-only and read-write segments split */
+    MH_LAZY_INIT = 0x40, /* the shared library init routine is to be run lazily via catching memory
+                        faults to its writeable segments (obsolete) */
+    MH_TWOLEVEL = 0x80,    /* the image is using two-level name space bindings */
+    MH_FORCE_FLAT = 0x100, /* the executable is forcing all images to use flat name space bindings */
+    MH_NOMULTIDEFS = 0x200, /* this umbrella guarantees no multiple defintions of symbols in its
+                           sub-images so the two-level namespace hints can always be used. */
+    MH_NOFIXPREBINDING = 0x400, /* do not have dyld notify the prebinding agent about this executable */
+    MH_PREBINDABLE = 0x800, /* the binary is not prebound but can have its prebinding redone. only used
+                            when MH_PREBOUND is not set. */
+    MH_ALLMODSBOUND = 0x1000, /* indicates that this binary binds to all two-level namespace modules of
+                              its dependent libraries. only used when MH_PREBINDABLE and MH_TWOLEVEL
+                              are both set. */
+    MH_SUBSECTIONS_VIA_SYMBOLS = 0x2000, /* safe to divide up the sections into sub-sections via symbols for dead
+                                         code stripping */
+    MH_CANONICAL = 0x4000, /* the binary has been canonicalized via the unprebind operation */
+    MH_WEAK_DEFINES = 0x8000, /* the final linked image contains external weak symbols */
+    MH_BINDS_TO_WEAK = 0x10000, /* the final linked image uses weak symbols */
+    MH_ALLOW_STACK_EXECUTION = 0x20000, /* When this bit is set, all stacks in the task will be given stack
+                                        execution privilege.  Only used in MH_EXECUTE filetypes. */
+    MH_ROOT_SAFE = 0x40000, /* When this bit is set, the binary declares it is safe for use in
+                            processes with uid zero */
+    MH_SETUID_SAFE = 0x80000, /* When this bit is set, the binary declares it is safe for use in
+                              processes when issetugid() is true */
+    MH_NO_REEXPORTED_DYLIBS = 0x100000, /* When this bit is set on a dylib, the static linker does not need to
+                                        examine dependent dylibs to see if any are re-exported */
+    MH_PIE = 0x200000, /* When this bit is set, the OS will load the main executable at a
+                       random address.  Only used in MH_EXECUTE filetypes. */
+    MH_DEAD_STRIPPABLE_DYLIB = 0x400000, /* Only for use on dylibs.  When linking against a dylib that
+                                         has this bit set, the static linker will automatically not create a
+                                         LC_LOAD_DYLIB load command to the dylib if no symbols are being
+                                         referenced from the dylib. */
+    MH_HAS_TLV_DESCRIPTORS = 0x800000, /* Contains a section of type S_THREAD_LOCAL_VARIABLES */
+    MH_NO_HEAP_EXECUTION = 0x1000000, /* When this bit is set, the OS will run the main executable with
+                                      a non-executable heap even on platforms (e.g. i386) that don't
+                                      require it. Only used in MH_EXECUTE filetypes. */
+    MH_APP_EXTENSION_SAFE = 0x02000000, /* The code was linked for use in an application extension. */
+    MH_NLIST_OUTOFSYNC_WITH_DYLDINFO = 0x04000000, /* The external symbols listed in the nlist symbol table do
+                                                   not include all the symbols listed in the dyld info. */
+    MH_SIM_SUPPORT = 0x08000000, /* Allow LC_MIN_VERSION_MACOS and LC_BUILD_VERSION load commands with
+                                 the platforms macOS, macCatalyst, iOSSimulator, tvOSSimulator and
+                                 watchOSSimulator. */
+    MH_IMPLICIT_PAGEZERO = 0x10000000, /* main executable has no __PAGEZERO segment.  Instead, loader (xnu)
+                                       will load program high and block out all memory below it. */
+    MH_DYLIB_IN_CACHE = 0x80000000, /* Only for use on dylibs. When this bit is set, the dylib is part of the dyld
+                                    shared cache, rather than loose in
+                                    the filesystem. */
 }
 
 pub fn parse(data: &[u8]) -> Result<Macho, String> {
@@ -63,8 +111,21 @@ fn parse_header(reader: &mut DataReader) -> Result<Header, String> {
         return Err("not a mach-o 64 file".to_string());
     }
     let cpu_type = parse_cpu_type(reader.read_i32())?;
+    let cpu_sub_type = parse_cpu_sub_type(cpu_type, reader.read_i32())?;
+    let file_type = parse_file_type(reader.read_u32())?;
+    let no_cmds = reader.read_u32() as usize;
+    let size_of_cmds = reader.read_u32() as usize;
+    let flags = parse_header_flags(reader.read_u32())?;
+    reader.skip(4); // reserved
 
-    Ok(Header { cpu_type })
+    Ok(Header {
+        cpu_type,
+        cpu_sub_type,
+        file_type,
+        no_cmds,
+        size_of_cmds,
+        flags,
+    })
 }
 
 fn parse_cpu_type(v: i32) -> Result<CPUType, String> {
@@ -74,6 +135,67 @@ fn parse_cpu_type(v: i32) -> Result<CPUType, String> {
         Ok(CPUType::ARM64)
     } else {
         Err(format!("unsupported cpu_type: {:x}", v))
+    }
+}
+
+fn parse_cpu_sub_type(cpu_type: CPUType, v: i32) -> Result<CPUSubType, String> {
+    if cpu_type == CPUType::ARM64 {
+        match v {
+            0 => Ok(CPUSubType::ARM_ALL),
+            _ => Err(format!("unsupported ARM64 cpu_sub_type: {:x}", v)),
+        }
+    } else {
+        Err(format!("unsupported cpu_sub_type: {:x}", v))
+    }
+}
+
+fn parse_file_type(v: u32) -> Result<FileType, String> {
+    match v {
+        0x1 => Ok(FileType::MH_OBJECT),
+        0x2 => Ok(FileType::MH_EXECUTED),
+        _ => Err(format!("unsupported file_type: {:x}", v)),
+    }
+}
+
+fn parse_header_flags(v: u32) -> Result<Vec<HeaderFlag>, String> {
+    let mut result = Vec::new();
+    let r = &mut result;
+    h_flag(v, HeaderFlag::MH_NOUNDEFS, r);
+    h_flag(v, HeaderFlag::MH_INCRLINK, r);
+    h_flag(v, HeaderFlag::MH_DYLDLINK, r);
+    h_flag(v, HeaderFlag::MH_BINDATLOAD, r);
+    h_flag(v, HeaderFlag::MH_PREBOUND, r);
+    h_flag(v, HeaderFlag::MH_SPLIT_SEGS, r);
+    h_flag(v, HeaderFlag::MH_LAZY_INIT, r);
+    h_flag(v, HeaderFlag::MH_TWOLEVEL, r);
+    h_flag(v, HeaderFlag::MH_FORCE_FLAT, r);
+    h_flag(v, HeaderFlag::MH_NOMULTIDEFS, r);
+    h_flag(v, HeaderFlag::MH_NOFIXPREBINDING, r);
+    h_flag(v, HeaderFlag::MH_PREBINDABLE, r);
+    h_flag(v, HeaderFlag::MH_ALLMODSBOUND, r);
+    h_flag(v, HeaderFlag::MH_SUBSECTIONS_VIA_SYMBOLS, r);
+    h_flag(v, HeaderFlag::MH_CANONICAL, r);
+    h_flag(v, HeaderFlag::MH_WEAK_DEFINES, r);
+    h_flag(v, HeaderFlag::MH_BINDS_TO_WEAK, r);
+    h_flag(v, HeaderFlag::MH_ALLOW_STACK_EXECUTION, r);
+    h_flag(v, HeaderFlag::MH_ROOT_SAFE, r);
+    h_flag(v, HeaderFlag::MH_SETUID_SAFE, r);
+    h_flag(v, HeaderFlag::MH_NO_REEXPORTED_DYLIBS, r);
+    h_flag(v, HeaderFlag::MH_PIE, r);
+    h_flag(v, HeaderFlag::MH_DEAD_STRIPPABLE_DYLIB, r);
+    h_flag(v, HeaderFlag::MH_HAS_TLV_DESCRIPTORS, r);
+    h_flag(v, HeaderFlag::MH_NO_HEAP_EXECUTION, r);
+    h_flag(v, HeaderFlag::MH_APP_EXTENSION_SAFE, r);
+    h_flag(v, HeaderFlag::MH_NLIST_OUTOFSYNC_WITH_DYLDINFO, r);
+    h_flag(v, HeaderFlag::MH_SIM_SUPPORT, r);
+    h_flag(v, HeaderFlag::MH_IMPLICIT_PAGEZERO, r);
+    h_flag(v, HeaderFlag::MH_DYLIB_IN_CACHE, r);
+    Ok(result)
+}
+
+fn h_flag(v: u32, flag: HeaderFlag, result: &mut Vec<HeaderFlag>) {
+    if (v & (flag as u32)) != 0 {
+        result.push(flag)
     }
 }
 
